b0ce016df7c98e126cc47142e735baabb77758a8
Muhammad_Zaki <165334996+muhammadzaki791@users.noreply.github.com>
2024-06-22 02:57:18 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,93 @@
+import inquirer from "inquirer";
+const ansewrOfuser :{
+    Sentence:string
+} = await inquirer.prompt  ([
+    {
+        name:"Sentence",
+        type:"input",
+        message:"Enter Your sentence to conunt the number of words : "
+        
+    }
+])
+let numofwords = ansewrOfuser.Sentence.trim().split(" ")
+// This will print a words of a sentence in a array.
+// console.log(numofwords);
+// This will print the number of words in a sentence.
+console.log(`The number of words your sentence contain is: ${numofwords.length}.`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
