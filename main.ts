171585c08a0d499b8cdfdc62a6a484f818724de8
Muhammad_Zaki <165334996+muhammadzaki791@users.noreply.github.com>
2024-06-25 03:55:24 -0700

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
 import inquirer from "inquirer";
+import chalk from "chalk"
 const ansewrOfuser :{
     Sentence:string
 } = await inquirer.prompt  ([
     {
         name:"Sentence",
         type:"input",
-        message:"Enter Your sentence to conunt the number of words : "
+        message:chalk.italic.bold.yellowBright("Enter Your sentence to conunt the number of words : ")
         
     }
 ])
@@ -13,7 +14,7 @@ let numofwords = ansewrOfuser.Sentence.trim().split(" ")
 // This will print a words of a sentence in a array.
 // console.log(numofwords);
 // This will print the number of words in a sentence.
-console.log(`The number of words your sentence contain is: ${numofwords.length}.`);
+console.log(chalk.italic.bold.bgBlack.yellow(`The number of words your sentence contain is: ${numofwords.length}.`));
 
 
 
